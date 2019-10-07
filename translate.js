06125693b7bd5c371cf819890d8296a0948fed17
Michael Jack <michaeljack255@gmail.com>
2019-10-08 00:34:59 +0900

    upload System translation

---
+++ translate.js
@@ -0,0 +1,81 @@
+//translate into japanese
+var aLangKeys=new Array();
+aLangKeys['en']=new Array();
+aLangKeys['jp']=new Array();
+//english
+aLangKeys['en']['home']='HOME';
+aLangKeys['en']['about']='ABOUT';
+aLangKeys['en']['portfolio']='PORTFOLIO';
+aLangKeys['en']['skills']='SKILL';
+aLangKeys['en']['contact']='CONTACT';
+aLangKeys['en']['contactme']='Contact Me';
+aLangKeys['en']['telp']='Telp:(080)-8743-0802';
+aLangKeys['en']['email']='Email: michaeljack255@gmail.com';
+aLangKeys['en']['school']='Tokyo Cool Japan College';
+aLangKeys['en']['myname']='MICHAEL JACK';
+aLangKeys['en']['job']='Game Programmer | Server Engineer';
+aLangKeys['en']['introduction']='After graduated from University, I entered  IT Corporation in Indonesia. My roles was responsible for troubleshooting and resolving technical issues, and also maintenance server. I’ve been at my company for two years now and have learned a lot from that company. However, i’m starting to feel like i need some new challenges. So, i chose study abroad program in Japan. Now, I’m studying game development and creating many projects from different background.';
+aLangKeys['en']['skill']='My Skill';
+aLangKeys['en']['fullname']='Full Name';
+aLangKeys['en']['mail']='Email';
+aLangKeys['en']['message']='Message';
+aLangKeys['en']['submit']='Submit';
+aLangKeys['en']['furpunk']='This group project was created by Unreal Engine. My roles is create a player to movement or shoot and also make a camera smooth between multiplayer. Repository manager used Git. Genre Game is Co-op Multiplayer shooting game. It will be released on February 2019.';
+aLangKeys['en']['wizardrun']='The second version of my first 2D game by Unity Engine(Unity 2018.3.0f1), this game have nearly finished, however you can play the game as well. This game like endless running and get a high score.';
+aLangKeys['en']['fps']='This game is the tutorial first person shooter. Use your skill or tactics and sharp shooter aim to defeat all of your enemies on the battlefield. This game feature is kill enemies and gain score points.This game made with Unity and released on PC only.';
+aLangKeys['en']['horror']='Horror game genre, the essence has always been fear.It was made by Unreal Engine and also substance material. The purpose of this game is find a key to get out from haunted house.';
+aLangKeys['en']['webPortfolio']='This portfolio website was created for showcase my works. The source code of this website hosted directly in GitHub repository.';
+aLangKeys['en']['fruit']='This personal project was created by Unity. The genre concept is puzzle game. This game use assets from the assets store in my game.';
+aLangKeys['en']['pinatata']='This group project was created by Unity. When GameJam, our team would make a simple and fun game within 2 days. My roles is make a UI/UX Design. The repository manager used Git.';
+aLangKeys['en']['projects']='Projects';
+aLangKeys['en']['sub-projects']='I started my personal projects in 2018 as student in Japan. My personal works are using Unity Game Engine. And also, The below link will direct you to view source code on GitHub.';
+aLangKeys['en']['dialog1']='This game was created by Unity Engine,The purpose is to learn and play the basis for a solid First Person Shooter.';
+aLangKeys['en']['dialog2']='2D Puzzle Game, inspired by Disney Tsum Tsum. So I make a simple game and fun.';
+aLangKeys['en']['button']='View On GitHub';
+
+
+//japanese
+aLangKeys['jp']['home']='ホーム';
+aLangKeys['jp']['about']='自己紹介';
+aLangKeys['jp']['portfolio']='ポートフォリオ';
+aLangKeys['jp']['skills']='技術的';
+aLangKeys['jp']['contact']='連絡';
+aLangKeys['jp']['contactme']='連絡先';
+aLangKeys['jp']['telp']='携帯:(080)-8743-0802';
+aLangKeys['jp']['email']='メール: michaeljack255@gmail.com';
+aLangKeys['jp']['school']='専門学校東京クールジャパン';
+aLangKeys['jp']['myname']='マイケル　ジャック';
+aLangKeys['jp']['job']='ゲーム　プログラマー | サーバー　エンジニア';
+aLangKeys['jp']['introduction']='大学卒業後、インドネシアでIT系の会社を働めました。 仕事の内容はサーバを管理するや技術的問題を解決です。 会社に2年勤めていたので、色々な経験をしました。でも、まだチャレンジしたいと思います。そして、日本への留学を決意しました。 今は東京ネットウェイブのゲームプログラマー専攻で勉強しています。そして様々なゲームを作りました。';
+aLangKeys['jp']['skill']='スキル';
+aLangKeys['jp']['fullname']='お名前';
+aLangKeys['jp']['mail']='メールアドレス';
+aLangKeys['jp']['message']='お問い合わせ内容';
+aLangKeys['jp']['submit']='送信';
+aLangKeys['jp']['furpunk']='このチーム制作は「Unreal Engine」によって作成されました。 私の役は、動きや撃つプレーヤーを作ることやそしてマルチプレイヤの間でカメラを滑らかにすることです。「GIT」ギットを使用しました。 ジャンルゲームはマルチプレイヤーシューティングゲームです。 2019年2月にリリースされます。';
+aLangKeys['jp']['wizardrun']='「Unity Engine」によると最初の2Dゲームの第2版（Unity 2018.3.0f1）、このゲームはほぼ終了しましたが、このゲームをプレイすることができます。 このゲームはラン＆ジャンプゲームのようにアイデアを考えたが、ハイスコアを得ます。';
+aLangKeys['jp']['fps']='このゲームはチュートリアルのシューティングゲームです。 スキルや戦術を使い、鋭い射手を使って戦場であなたのすべての敵を倒してください。 このゲームの特徴は敵を殺して得点を稼ぐことです。このゲームはUnityで作られ、PCでのみリリースされています。';
+aLangKeys['jp']['horror']='ホラーゲームのジャンル、その本質は常に恐れられてきました。それはUnreal Engineによって作られた。 このゲームの目的は、お化け屋敷から出るための鍵を見つけることです。'
+aLangKeys['jp']['webPortfolio']='このポートフォリオのサイトは私の作品を紹介するために作成されました。 このサイトのコードは「GIT」リポジトリに直接ホストされています。';
+aLangKeys['jp']['fruit']='個人的なプロジェクトでUnityを作成されました。 ジャンルのコンセプトはパズルゲームです。 このゲームのUIはアセットストアからのアセットを使用します。';
+aLangKeys['jp']['pinatata']='グループプロジェクトでUnityを作成されました。 ゲームジャムのとき、私たちのチームは2日以内にシンプルで楽しいゲームを作ると考えました。 私の管理はUI / UXデザインを作ることです。 リポジトリマネージャはGitを使用しました。';
+aLangKeys['jp']['projects']='作品について';
+aLangKeys['jp']['sub-projects']='学生時代はゲームを制作し、Unityゲームを使用しました。以下ボタンを押すとGITHUBウェブサイトに移動するになります。';
+aLangKeys['jp']['dialog1']='このゲームはUnityゲームエンジンを使用して、目的は初心者の方が楽しみプレイすることです';
+aLangKeys['jp']['dialog2']='ディズニーツムツムから触発されて２Dパズルゲームを制作しました。楽しみだけでなくシンプルなゲームを作て誰でも遊ぶことができるの目標です。';
+aLangKeys['jp']['button']='ソースコードを表示';
+
+//jquery
+$(document).ready(function() {
+	// onclick behavior
+	$('.lang').click( function() {
+	var lang =  $(this).attr('id');// obtain language id
+
+	// translate all translatable elements
+	$('.tr').each(function(i){
+		$(this).text(aLangKeys[lang][ $(this).attr('key') ]);
+	});
+	} );
+});
+
+
